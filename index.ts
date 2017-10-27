d362676f8e3f2fb7a00aa55c6c2c915d083ade40
sdeprez <sdeprez@users.noreply.github.com>
2017-10-27 18:16:43 +0200

    Remove default form options

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import DataGenerator from './app/scripts.babel/form-filler/data-generator.js'
-import {FormFillerDefaultOptions} from './app/scripts.babel/form-filler/helpers.js'
 
 
 export {
   DataGenerator,
-  FormFillerDefaultOptions,
 }
