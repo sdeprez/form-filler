cb48e1d7189b61b8a837082a01d47003209c74d2
sdeprez <sdeprez@users.noreply.github.com>
2017-10-27 18:12:43 +0200

    Add index.ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+import DataGenerator from './app/scripts.babel/form-filler/data-generator.js'
+import {FormFillerDefaultOptions} from './app/scripts.babel/form-filler/helpers.js'
+
+
+export {
+  DataGenerator,
+  FormFillerDefaultOptions,
+}
